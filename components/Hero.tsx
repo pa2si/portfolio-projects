6ecd10c9531b84d1da263551811978cd40beb81b
pa2si <pascaliscoding@gmail.com>
2024-06-26 10:08:25 +0200

    hero bg not responsive

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,12 @@ import Link from "next/link";
 
 const Hero = () => {
   return (
-    <section className="bg-hero hero h-svh">
+    <section
+      className="hero h-svh"
+      style={{
+        backgroundImage: "url(/oskar-yildiz-cOkpTiJMGzA-unsplash.jpg)",
+      }}
+    >
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
